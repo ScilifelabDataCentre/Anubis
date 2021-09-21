261d667b4b2efc82b2abd3b1a75b1d302e5dde44
Per Kraulis <per.kraulis@gmail.com>
2021-09-21 15:36:58 +0200

    Added browser login test.

--- tests/browser_anonymous.py
+++ tests/browser_anonymous.py
@@ -14,12 +14,11 @@ class Homepage(utils.BrowserTestCase):
 
     def test_home(self):
         "Test presence of table in home page and version indicator."
-        print(self.settings["BASE_URL"])
         self.driver.get(self.settings["BASE_URL"])
 
         # Software versions displayed in footer in every page.
         elem = self.driver.find_element_by_id("version")
-        self.assertGreaterEqual(elem.text, "1.3.15")
+        self.assertGreaterEqual(elem.text, "1.3.17")
 
 
 if __name__ == "__main__":
