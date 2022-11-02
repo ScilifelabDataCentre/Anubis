9ba5f891546803f97a2a6068bb040198df0d8607
Per Kraulis <per.kraulis@scilifelab.se>
2022-11-02 13:42:25 +0100

    Fix CLI dump -D bug. Closes #556.

--- anubis/cli.py
+++ anubis/cli.py
@@ -198,7 +198,7 @@ def dump(dumpfile, dumpdir, progressbar):
         if not dumpfile:
             dumpfile = "dump_{0}.tar.gz".format(time.strftime("%Y-%m-%d"))
             if dumpdir:
-                filepath = os.path.join(dumpdir, dumpfile)
+                dumpfile = os.path.join(dumpdir, dumpfile)
         ndocs, nfiles = flask.g.db.dump(
             dumpfile, exclude_designs=True, progressbar=progressbar
         )
